d3ebd8c675be2923fdc67ae4b6579c42de1ebf7b
nkina1516 <157325721+nkina1516@users.noreply.github.com>
2025-02-08 20:16:50 -0600

    added some files

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public static float     bottomY = -20f;  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( transform.position.y < bottomY ) {
+            Destroy( this.gameObject ); 
+            // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();           // b
+            // Call the public AppleMissed() method of apScript
+            apScript.AppleMissed();
+              
+        }
+
+    }
+}
